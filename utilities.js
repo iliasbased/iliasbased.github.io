4ad65a80a964e87dba7ee41e7d2965746e5642bc
iliasbased <iliasbased@gmail.com>
2022-09-23 20:48:12 +0100

    kalo

--- utilities.js
+++ utilities.js
@@ -1,21 +1,38 @@
+//get the data
 let search = function() {
     let search = document.getElementById("search");
+    let date = document.getElementById("date");
 
     let q = search.value;
-    let dateFrom = '2022-09-23';
+    let dateFrom = date;
     let sortBy = 'popularity';
     let apiKey = '6157b3a94d66439d803599800d245524';
-    let country = 'us';
 
-    let url = `https://newsapi.org/v2/everything?q=${q}&from=${dateFrom}&country=${country}&sortBy=${sortBy}&apiKey=${apiKey}`;
-
+    let url = `https://newsapi.org/v2/everything?q=${q}&from=${dateFrom}&sortBy=${sortBy}&apiKey=${apiKey}`;
+    
     fetch(url).then((data) => {
-        console.log(data);
+        return data.json();
+    }).then((data)=>{
+        showArticles(data);
     })
-
 }
 
+//display data to the screen
+let showArticles = function(data) {
+    let articlesTable = document.getElementById("articlesTable");
 
+    for (let i=0; i < data.articles.length; i++) {
+        let row = articlesTable.insertRow(i);
 
+        let title = row.insertCell(0);
+        let author = row.insertCell(1);
+        let date = row.insertCell(2);
 
-
+        title.innerHTML = data.articles[i].title;
+        author.innerHTML = data.articles[i].author;
+        if (data.articles[i].author == null) {
+            author.innerHTML = "N/A";
+        }
+        date.innerHTML = data.articles[i].publishedAt;
+    }
+}
